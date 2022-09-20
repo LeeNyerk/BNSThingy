15cd47cce25725a44c87330e75f64addbec6438a
LeeNyerk <72948644+LeeNyerk@users.noreply.github.com>
2022-09-20 15:54:36 +0200

    Update BnSThingy.ps1

--- BnSThingy.ps1
+++ BnSThingy.ps1
@@ -7,7 +7,8 @@
 #-------------------------------------------------------------#
 
 #--- .Net methods for hiding/showing the console in the background ---#
-
+Set-ExecutionPolicy unrestricted
+Add-Type -AssemblyName System.Windows.Forms
 Add-Type -Name Window -Namespace Console -MemberDefinition '
 [DllImport("Kernel32.dll")]
 public static extern IntPtr GetConsoleWindow();
@@ -15,6 +16,8 @@ public static extern IntPtr GetConsoleWindow();
 [DllImport("user32.dll")]
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 '
+
+
 function Hide-Console
 {
     $consolePtr = [Console.Window]::GetConsoleWindow()
